037078bd0824fdcb5e5053e6cd87753e4ffd5d30
NiveSJ <niveathithavj@gmail.com>
2022-11-02 10:01:12 +0100

    Added attributes and methods to class.

---
+++ src/main/java/se/lexicon/Book.java
@@ -0,0 +1,14 @@
+package se.lexicon;
+
+public class Book {
+    private String title;
+    private String author;
+    private boolean available;
+
+
+    public Book showPerson() {
+
+
+    }
+
+}
---
+++ src/main/java/se/lexicon/Person.java
@@ -0,0 +1,29 @@
+package se.lexicon;
+
+public class Person {
+    private int personId;
+    private String firstName;
+    private String lastName;
+
+    public Person(int personId, String firstName, String lastName) {
+
+        this.personId = personId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+    }
+
+    public void loanBooks(Book book) {
+
+    }
+
+    public String displayBooks() {
+
+        String book = null;
+
+        return book;
+
+
+    }
+
+}
